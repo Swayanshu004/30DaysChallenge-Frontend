3c9028751450d1f7fa762d0edd2eda30d594937e
Divyagyani <divyagyanisahu2004@gmail.com>
2024-09-06 21:47:46 +0530

    for pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
   return (
    <div>
 
-    <h1 className="font-bold text-5xl text-orange-600 m-5">
-      codingaashram
-    </h1>
-    <footer>createdby : Veterans</footer>
+   
    </div>
   );
 }
